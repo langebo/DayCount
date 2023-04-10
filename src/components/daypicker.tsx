96421111d37a5024e8a69ae1a1aefa0b0d451e29
langebo <bslange@yahoo.de>
2023-04-10 12:11:34 +0200

    added daypicker

---
+++ src/components/daypicker.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
+
+export interface DayPickerProps {
+  day: Date;
+  onDayChanged: (d: Date) => void;
+}
+
+const DayPicker: React.FC<DayPickerProps> = ({ day, onDayChanged: setDay }) => {
+  const CustomInput = (
+    props: React.HTMLProps<HTMLInputElement>,
+    _ref: React.ForwardedRef<HTMLInputElement>
+  ) => {
+    return (
+      <input
+        {...props}
+        className="m-0 w-40 border-b-2 p-0 text-3xl text-blue-400 focus:border-blue-400 focus:outline-none"
+      />
+    );
+  };
+
+  return (
+    <DatePicker
+      selected={day}
+      onChange={setDay}
+      minDate={new Date()}
+      dateFormat="dd.MM.yyyy"
+      className="m-0 p-0"
+      customInput={React.createElement(React.forwardRef(CustomInput))}
+    />
+  );
+};
+
+export default DayPicker;
